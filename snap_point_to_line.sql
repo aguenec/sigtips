b2a1135d570a28e851bf21e8b7a252f912fb23b2
aguenec <63242069+aguenec@users.noreply.github.com>
2022-01-12 13:49:44 +0100

    Update snap_point_to_line.sql

--- snap_point_to_line.sql
+++ snap_point_to_line.sql
@@ -12,11 +12,11 @@ FROM
         ln.geom AS ln_geom,
         pt.geom AS pt_geom, 
         ln.cleabs AS ln_id, 
-        pt.pt_collecte_id AS pt_id, 
+        pt.pt_id AS pt_id, 
         ST_Distance(ln.geom, pt.geom) AS d
     FROM 
-        qc_collecte.point_collecte pt, 
-        ref_bd_topo_qc.troncon_de_route ln 
+        point pt, 
+        line ln 
     WHERE 
         ST_DWithin(pt.geom, ln.geom, 50.0) 
     ORDER BY pt_id, d
